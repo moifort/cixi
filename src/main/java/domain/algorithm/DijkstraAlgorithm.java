a446c4c2a6aad55b4a8bce9c1a6f17bc35c1a8b9
Thibaut Mottet <thibaut.mottet@gmail.com>
2019-10-07 22:26:40 +0200

    WIP

--- src/main/java/domain/algorithm/DijkstraAlgorithm.java
+++ src/main/java/domain/algorithm/DijkstraAlgorithm.java
@@ -3,6 +3,7 @@ package domain.algorithm;
 import domain.City;
 import domain.Road;
 import domain.algorithm.dijkstra.Dijkstra;
+import domain.algorithm.dijkstra.Vertex;
 
 import java.util.List;
 
@@ -11,7 +12,27 @@ public class DijkstraAlgorithm implements ShortestTrackAlgorithm {
 
     @Override
     public List<Road> shortestTrack(City from, City to, List<Road> roads) {
-        throw new RuntimeException("Not Implemented Yet");
+        List<Vertex> vertex = toVertex(roads);
+        Vertex start = findStartVertex(from, vertex);
+        Vertex end = findEndVertex(to, vertex);
+        List<Vertex> shortestTrack = dijkstra.shortestPathTo(start, end);
+        return toRoad(shortestTrack);
+    }
+
+    private List<Road> toRoad(List<Vertex> shortestTrack) {
+        return null;
+    }
+
+    private Vertex findEndVertex(City to, List<Vertex> vertex) {
+        return null;
+    }
+
+    private Vertex findStartVertex(City from, List<Vertex> vertex) {
+        return null;
+    }
+
+    private List<Vertex> toVertex(List<Road> roads) {
+        return null;
     }
 
 }
