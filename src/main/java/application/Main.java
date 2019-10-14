b1cc6a1c01ebdd437a447301f05bdb261803286b
moifort <Moifort91!>
2019-10-14 11:33:43 +0200

    GUI

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -7,10 +7,13 @@ public class Main {
             new SimulatorApplication().run();
         } else if (args[0].equals("cli")) {
             new CliApplication().run();
+        } else if (args[0].equals("gui")) {
+            new GuiApplication().run();
         } else {
             System.out.println("Help:");
             System.out.println(" - no argument run the simulator");
-            System.out.println(" - 'cli' argument run the command-line interface");
+            System.out.println(" - 'cli' argument run command-line interface");
+            System.out.println(" - 'gui' argument run graphical user interface");
         }
     }
 }
